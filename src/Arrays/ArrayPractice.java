2583325b04d5f3fb6385cc569e2fc2fdbd9bb610
Shraddha <shraddhasavadi1803@gmail.com>
2025-11-06 12:36:47 +0530

    Added comment

--- src/Arrays/ArrayPractice.java
+++ src/Arrays/ArrayPractice.java
@@ -8,7 +8,12 @@ public class ArrayPractice {
 
     {
         int arr [] ={1,2,3,4,5,6};
-
+// This is for each loop , here no need have increment operation or condition
+//
+//
+//
+//
+// this is additional comment
         for (int x:arr){
             System.out.println(x);
     }
