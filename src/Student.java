19d8401662ff18e5c1d8ecca8b43ce786e71fff3
Shraddha <shraddhasavadi1803@gmail.com>
2025-11-06 17:56:35 +0530

    added comments

--- src/Student.java
+++ src/Student.java
@@ -2,12 +2,14 @@ public class Student {
     int rollNo, marks;
     String name;
 
+    //  parametrized constructor
     Student(int rollNO,int marks,String name) {
         this.rollNo = rollNO;
         this.marks = marks;
         this.name = name;
     }
 
+    //method to diplay student details
     void display() {
         System.out.println("Printing the details of the student :");
         System.out.println("Name:"+ name);
@@ -17,9 +19,12 @@ public class Student {
 
     }
         public static void main (String[]args){
+
+            // Create 2 students using parameterized constructor
             Student st1 = new Student(11, 99,"Sima");
             Student st2=new Student(12,98,"Mira");
 
+            // Display their info
             st1.display();
             st2.display();
         }
