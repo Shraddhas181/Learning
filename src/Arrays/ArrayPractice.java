43481b4d19d4888755f094c0641e8f66f5c7971c
Shraddha <shraddhasavadi1803@gmail.com>
2025-11-06 12:33:27 +0530

    additional changes

---
+++ src/Arrays/ArrayPractice.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.sql.SQLOutput;
+
+public class ArrayPractice {
+
+    public void testarray ()
+
+    {
+        int arr [] ={1,2,3,4,5,6};
+
+        for (int x:arr){
+            System.out.println(x);
+    }
+
+
+    }
+}
